a8a0458c7c544d7bf859bd82a1d97417e194f49d
Avirup Basu <ami.avirup17@gmail.com>
2023-11-30 10:54:23 +0530

    Some changes

--- core/generate_qr.py
+++ core/generate_qr.py
@@ -1,6 +1,10 @@
 import qrcode
 import image
-import PIL
+from PIL import Image, ImageDraw
+
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.moduledrawers import CircleModuleDrawer
+from  qrcode.image.styles.colormasks import SolidFillColorMask
 
 class GenerateQrCode():
     def __init__(self) -> None:
@@ -9,12 +13,29 @@ class GenerateQrCode():
         box_size=12,
         border=2)
 
-    def generateQrFromUrl(self,url):
+    def generateQrFromUrl(self,url,isRounded):
         self.qr.add_data(url)
         self.qr.make(fit=True)
         img=self.qr.make_image(fill = 'Black',back_color = 'White')
-        return img
-    
-    
+        if(not isRounded):
+            return img
+        # else:
+        #     # Make the edges rounded
+        #     width, height = img.size
+        #     mask = Image.new("L", (width, height), 0)
+        #     draw = ImageDraw.Draw(mask)
+            
+        #     img = Image.composite(img, Image.new("RGB", img.size, "white"), mask)
+        #     return img  
+
+
+def main():
+    url='https://www.linkedin.com/in/avirup171/'
+    gqr=GenerateQrCode()
+    img=gqr.generateQrFromUrl(url,False)
+    img.save("img.png")
+
+if __name__=='__main__':
+    main()
 
     
